8104064049e2fed6669a3c4d679ea23ca76e06b3
Andrey Tumanov <130775922+Andrey-Fog@users.noreply.github.com>
2023-05-08 17:53:28 +0300

    Update usermat3d.f

--- Source/usermat3d.f
+++ Source/usermat3d.f
@@ -14,7 +14,7 @@ c
 c     Custom user material subroutine for Conventional Gradient Plasticity Model.      
 c     The procedure is created for SOLID186 element with Keyopt(2)=0.
 c     SOLID186 have a 8 integration points. 
-c     Coordinates of integration points in the coordinate system s-t-r = ± 0.577350269189626
+c     Coordinates of integration points in the coordinate system s-t-r = Â± 0.577350269189626
 !DEC$ ATTRIBUTES DLLEXPORT, ALIAS:"USERMAT3D"::usermat3d 
       use kplastic
 c
@@ -475,7 +475,7 @@ c     lines are s t r
         eta(10)=b1*SVAR(1,4) + b2*SVAR(2,4) + b3*SVAR(3,4)
      1        + b4*SVAR(4,4) + b5*SVAR(5,4) + b6*SVAR(6,4) 
      2        + b7*SVAR(7,4) + b8*SVAR(8,4) 
-        !dn212 !ó íåãî ïî÷åìó-òî ìèíóñ
+        !dn212 !Ã³ Ã­Ã¥Ã£Ã® Ã¯Ã®Ã·Ã¥Ã¬Ã³-Ã²Ã® Ã¬Ã¨Ã­Ã³Ã±
         eta(11)=a1*SVAR(1,5) + a2*SVAR(2,5) + a3*SVAR(3,5)
      &        + a4*SVAR(4,5) + a5*SVAR(5,5) + a6*SVAR(6,5) 
      &        + a7*SVAR(7,5) + a8*SVAR(8,5)
@@ -780,67 +780,4 @@ c
       statev(2)=coords(2)
       statev(3)=coords(3)
       return
-      end
-
-      
-      subroutine elasticstiffnes(Ex, nu, mcomp, outmatrix)
-c *** subroutine return the elastic stiffnes matrix
-c
-c *** Input arguments 
-c         Ex                          - Young's modulus
-c         nu                          - Poisson's ratio
-c         mcomp                       - array dimension     
-c                                       (4 - for plane strain, 6 - for full 3d)
-c *** Output arguments 
-c         outmatrix(mcomp,mcomp)      - Output stiffnes matrix
-c
-c ***     local variables
-c     eg2     (dp)        - Two shear modulus 
-c
-c ***     Additional variables      
-c     tmpelam    (dp)        
-c
-      INTEGER icomp, jcomp, mcomp
-      DOUBLE PRECISION 
-     &                 Ex, nu, eg2, tmpelam
-      DOUBLE PRECISION outmatrix(mcomp,mcomp)
-c
-c***************** Body of the subroutine********************************   
-c
-          eg2=Ex/(1.d0+nu)
-          tmpelam=(Ex/(1.d0-2.d0*nu)-eg2)/3.d0            
-          outmatrix=0.d0
-          do icomp=1,3
-              do jcomp=1,3
-                  outmatrix(jcomp,icomp)=tmpelam
-              end do
-              outmatrix(icomp,icomp)=eg2+tmpelam
-          end do
-          do icomp=4, mcomp
-              outmatrix(icomp,icomp)=eg2/2.d0
-          end do      
-          return
-      end
-
-      subroutine calcenergy(StrainTens, StressTens, numcomp, sed)
-c *** subroutine return the elastic work
-c
-c *** Input arguments 
-c         StrainTens   (dp,ar(ncomp),i)       - Strain at beginning of time increment
-c         StressTens   (dp,ar(ncomp),i)       - Stress
-c         numcomp                             - array dimension     
-c                                       (4 - for plane strain, 6 - for full 3d)
-c *** Output arguments 
-c         sed  - elastic work      
-c
-      INTEGER numcomp
-      DOUBLE PRECISION StrainTens(numcomp), StressTens(numcomp), sed
-c
-c***************** Body of the subroutine********************************   
-c
-	  sed = 0
-	  do i = 1, ncomp
-              sed = sed + StrainTens(i)*StressTens(i)
-	  end do
-          return
       end
